31a1d58f4a844643cc56c406728283cd893569b1
Tarcísio Nunes <github@tarcnux.com.br>
2019-08-18 15:05:47 -0300

    Recebendo int retornando Json

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/")
-def api():
-    return jsonify({'nome': 'Tarcísio Nunes'})
+@app.route("/<int:id>")
+def api(id):
+    return jsonify({'id':id, 'nome': 'Tarcísio Nunes', 'profissao': 'professor'})
 
 if __name__ == "__main__":
     app.run(debug=True)
