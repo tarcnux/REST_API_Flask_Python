11a15016beea64ae1670b1f1b6354f9ee208f3a8
Tarcísio Nunes <github@tarcnux.com.br>
2019-08-18 15:10:55 -0300

    Operação aritmética com parâmetros na URI

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ app = Flask(__name__)
 
 @app.route("/<int:id>")
 def api(id):
-    return jsonify({'id':id, 'nome': 'Tarcísio Nunes', 'profissao': 'professor'})
+    return jsonify({'id': id, 'nome': 'Tarcísio Nunes', 'profissao': 'professor'})
+
+
+@app.route("/soma/<int:inteiro1>/<int:inteiro2>", methods=['POST'])
+def soma(inteiro1, inteiro2):
+    return jsonify({
+        'inteiro1': inteiro1,
+        'inteiro2': inteiro2,
+        'soma': inteiro1+inteiro2
+    })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
