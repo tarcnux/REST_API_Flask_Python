e90dde0a92d7f69362ea7816b391fa962b8fc28f
Tarcísio Nunes <github@tarcnux.com.br>
2019-08-18 23:55:49 -0300

    Verbos GET, PUT e DELETE

--- dev_api/api.py
+++ dev_api/api.py
@@ -4,13 +4,37 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+desenvolvedores = [
+    {"id": 0,
+        "nome": "Tarcísio Nunes",
+        "habilidades": ["Python", "Flask", "PHP", "MySQL", "HTML", "CSS"]},
+    {"id": 1,
+        "nome": "Tarcnux TNX",
+        "habilidades": ["Python", "Django"]},
+    {"id": 2,
+        "nome": "Guido van Rossum",
+        "habilidades": ["Python", "Django", "Flask"]}
+]
 
-class HelloWorld(Resource):
+
+class Desenvolvedor(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'Alo': 'Ha GET'}
+
+    def put(self):
+        return {'Alo': 'Ha PUT'}
+
+    def delete(self):
+        return {'Alo': 'Ha DELETE'}
 
 
-api.add_resource(HelloWorld, '/')
+class Tarcnux(Resource):
+    def get(self):
+        return {'Alo': 'Ha Tarcnux'}
+
+
+api.add_resource(Tarcnux, '/')
+api.add_resource(Desenvolvedor, '/v2/dev/')
 
 if __name__ == '__main__':
     app.run(debug=True)
