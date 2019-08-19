c3d78e66e0eb09ab5f0334f4200dd32e7221bae4
Tarcísio Nunes <github@tarcnux.com.br>
2019-08-19 10:40:33 -0300

    CRUD método consulta

--- crud_api/utils.py
+++ crud_api/utils.py
@@ -1,14 +1,18 @@
-from models import Pessoa, Atividade
+from models import Pessoa, Atividade, db_session
 
 
 def insere_pessoa():
     pessoa = Pessoa(nome='Tarcnux', idade=41)
     print(pessoa)
+    db_session.add(pessoa)
+    db_session.commit()
 
 
 def consulta():
-    pass
+    pessoa = Pessoa.query.all()
+    print(pessoa)
 
 
 if __name__ == '__main__':
-    insere_pessoa()
+    # insere_pessoa()
+    consulta()
